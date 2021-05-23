750f2a31b728859378c13d97456a854c62ffb8c3
SChau <71276205+SamsonChau@users.noreply.github.com>
2021-05-23 09:49:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "m3508.h"
 
 Timer t;
-InterruptIn userbutton(PC_13);
+InterruptIn userbutton(USER_BUTTON);
 CAN can1(PA_11, PA_12, 1000000);
 m3508 _m3508;
-int id = 1;
+int id = 0;
 
 void delay(int time)//delay in micro sec
 {
@@ -23,20 +23,21 @@ int main()
     // changing this may result it lost control, please ensure safty and clear the site before power up the device 
     
     // PID for position
-    _m3508.set_p_pid_param(id, 6.8,  0.195, 0.00055);
+    _m3508.set_p_pid_param(id, 2.74,  0.07, 0.00057);
     // PID for velocity for position loop
-    _m3508.set_v_pid_param(id, 2, 0.2, 0.00049);
+    _m3508.set_v_pid_param(id, 1, 0.01, 0.00049);
 
     // velocity set value, try to change it if the speed in not encough
     //** very sensitive 
-    _m3508.set_velocity(id,12000);
+    _m3508.set_velocity(id,0);
     //inital start up position
     _m3508.set_position(id,0);
 
     while(1) {
         if(!userbutton) {
             //set position to +1400
-            _m3508.set_position(id,1400);
+            _m3508.set_position(id,24510);
+            //_m3508.CAN_Send(5000,0,0,0);
         } else {
             //set position back to zero
             _m3508.set_position(id,0);
@@ -46,6 +47,6 @@ int main()
         _m3508.c620_write();
         
         //control the frequncy of for the PID controller 
-        delay(50);
+        delay(500);
     }
 }
